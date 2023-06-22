2360721f7fc92b10110c00507a942b80c057844d
Aliaksandr Vasilevich <>
2023-06-22 05:04:01 -0700

    fixingBugs

---
+++ NetflixDB/OnboardingScreen/DescriptionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DescriptionViewController.swift
+//  NetflixDB
+//
+//  Created by Aliaksandr Vasilevich on 6/22/23.
+//
+
+import Foundation
+import UIKit
+
+class DescriptionViewController: UIViewController {
+    
+}
